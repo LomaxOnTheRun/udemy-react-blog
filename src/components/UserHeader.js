0181f158518296710c1c181a55cebf9e2d8ce829
LomaxOnTheRun <lomax.on.the.run@gmail.com>
2019-01-04 15:57:38 +1100

    Create usersReducer

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -8,11 +8,22 @@ class UserHeader extends React.Component {
   }
 
   render() {
-    return <div>User Header</div>;
+    const { user } = this.props;
+
+    if (user) {
+      return <div className="header">{user.name}</div>;
+    }
+    return null;
   }
 }
 
+const mapStateToProps = (state, ownProps) => {
+  return {
+    user: state.users.find(user => user.id === ownProps.userId)
+  };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchUser }
 )(UserHeader);
